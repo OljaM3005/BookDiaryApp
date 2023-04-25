34156cfee6f621cf00cbb2b9f737523d0ee7fd28
OljaM3005 <olja3005@gmail.com>
2023-04-25 11:10:26 +0200

    removed code

--- app/src/main/java/com/example/bookdiary/Form.java
+++ app/src/main/java/com/example/bookdiary/Form.java
@@ -14,13 +14,6 @@ import android.widget.Spinner;
 
 public class Form extends AppCompatActivity {
 
-    private int indeks;
-    private My_book k;
-    private ArrayList<My_book> knjige;
-    ConstraintLayout podlayout;
-    EditText title, author, notes;
-    Spinner genre, status;
-    Intent i;
     Button save, quit;
 
     @Override
@@ -30,75 +23,9 @@ public class Form extends AppCompatActivity {
 
         save = findViewById(R.id.save_btn);
         quit = findViewById(R.id.give_up);
-
-        knjige = (ArrayList<My_book>)getIntent().getSerializableExtra("knjige");
-        indeks = getIntent().getIntExtra("indeks", -1);
-        if (indeks == -1){
-            addBook();
-        }
-        else{
-            editBook();
         }
     }
 
-    public void addBook(){
 
-        title = podlayout.findViewById(R.id.bookTitleId);
-        author = podlayout.findViewById(R.id.bookAuthorId);
-        save = podlayout.findViewById(R.id.save_btn);
-        quit = podlayout.findViewById(R.id.give_up);
 
-        k = new My_book("Title", "Author");
-        i = new Intent(Form.this, MainPage.class);
-        quit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i.putExtra("knjiga", knjige);
-                startActivity(i);
-                finish();
-            }
-        });
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //k.setTitle(title.getText().toString());
-                //k.setAuthor(author.getText().toString());
-                knjige.add(k);
-                i.putExtra("knjige", "knjige");
-                startActivity(i);
-                finish();
-            }
-        });
-    }
-
-    public void editBook(){
-        title = podlayout.findViewById(R.id.bookTitleId);
-        author = podlayout.findViewById(R.id.bookAuthorId);
-        save = podlayout.findViewById(R.id.save_btn);
-        quit = podlayout.findViewById(R.id.give_up);
-
-        k = knjige.get(indeks);
-
-        title.setText(k.getTitle());
-        author.setText(k.getAuthor());
-
-        i = new Intent(Form.this, MainPage.class);
-        quit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                i.putExtra("knjige", "knjige");
-                startActivity(i);
-            }
-        });
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               // knjige.get(indeks).setTitle(title.getText().toString());
-               // knjige.get(indeks).setAuthor(author.getText().toString());
-                i.putExtra("knjige", knjige);
-                startActivity(i);
-            }
-        });
-    }
-}
 
